b4842bb59cd333499897c04876cb434289dc9232
Ivan Goryaev <5474n.sage@gmail.com>
2025-07-19 20:33:02 +0300

    добавил передачу количества ключей при покупке

--- app/Http/Requests/KeyOrderRequest.php
+++ app/Http/Requests/KeyOrderRequest.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Http\FormRequest;
  *     @OA\Property(property="user_id", type="integer", description="Telegram ID пользователя"),
  *     @OA\Property(property="region_id", type="integer", description="ID региона"),
  *     @OA\Property(property="period_id", type="integer", description="ID периода"),
+ *     @OA\Property(property="quantity", type="integer", description="Количество ключей (от 1 до 5)", example=1, minimum=1, maximum=5)
  * ),
  */
 class KeyOrderRequest extends FormRequest
@@ -19,6 +20,7 @@ class KeyOrderRequest extends FormRequest
             'user_id' => 'required|integer|exists:users,telegram_id',
             'region_id' => 'required|integer|exists:regions,id',
             'period_id' => 'required|integer|exists:periods,id',
+            'quantity' => 'integer|min:1|max:5',
         ];
     }
 }
