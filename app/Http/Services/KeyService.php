82a5012654ebd56f15a048ffb91b1d407e5c7b80
Ivan Goryaev <5474n.sage@gmail.com>
2025-07-20 14:44:48 +0300

    добавил шифрование aes + rsa

--- app/Http/Services/KeyService.php
+++ app/Http/Services/KeyService.php
@@ -6,6 +6,8 @@ use App\Http\DTOs\KeyOrderDTO;
 use App\Http\Integrations\YooKassaService;
 use App\Models\Price;
 use App\Models\User;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Storage;
 
 class KeyService
 {
@@ -38,8 +40,62 @@ class KeyService
             'key' => $config['Identifier'],
         ]);
 
-        return $config;
+        $parsedConfig = $this->parseConfig($config);
+        return $this->encryptHybrid($parsedConfig);
     }
 
-    
+    public function encryptHybrid(string $config)
+    {
+        $aesKey = random_bytes(32);
+        $iv = random_bytes(16);
+
+        $encryptedData = openssl_encrypt($config, 'AES-256-CBC', $aesKey, OPENSSL_RAW_DATA, $iv);
+
+        $publicKey = openssl_pkey_get_public(Storage::get('client_public.pem'));
+
+        $encryptedKey = null;
+        if (!openssl_public_encrypt($aesKey, $encryptedKey, $publicKey)) {
+            throw new \Exception('Не удалось зашифровать AES-ключ');
+        }
+
+        return [
+            'key' => base64_encode($encryptedKey),
+            'data' => base64_encode($encryptedData),
+            'iv' => base64_encode($iv),
+        ];
+    }
+
+    public function parseConfig(Response $config)
+    {
+        $resultConfig = collect();
+        $resultConfig->push($this->getConfigInterface($config));
+        $resultConfig->push($this->getConfigPeer($config));
+
+        return implode("\n", $resultConfig->toArray());
+    }
+
+    public function getConfigInterface($config)
+    {
+        $interface = collect();
+        $interface->push('[Interface]');
+        $interface->push('Private key = ' . $config['PrivateKey']);
+        $interface->push('Address = ' . implode(',', $config['Addresses']));
+        $interface->push('DNS = ' . implode(',', $config['Dns']['Value']));
+        $interface->push('MTU = ' . $config['Mtu']['Value']);
+
+        return implode("\n", $interface->toArray());
+    }
+
+    public function getConfigPeer($config)
+    {
+        $peer = collect();
+        $peer->push('[Peer]');
+        $peer->push('PublicKey = ' . $config['PublicKey']);
+        $peer->push('Endpoint = ' . $config['Endpoint']['Value']);
+        $peer->push('AllowedIPs = ' . implode(',', $config['AllowedIPs']['Value']));
+        $peer->push('PresharedKey = ' . $config['PersistentKeepalive']['Value']);
+        $peer->push('PersistentKeepalive = ' . $config['PublicKey']);
+
+        return implode("\n", $peer->toArray());
+    }
 }
