bc786531a4721ad2de617c72c548a89fdc135c88
Ivan Goryaev <5474n.sage@gmail.com>
2025-08-26 21:18:39 +0300

    тест

--- app/Http/Integrations/YooKassaCallbackController.php
+++ app/Http/Integrations/YooKassaCallbackController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Integrations;
 
 use App\Http\Requests\YooKassa\YooKassaWebhookRequest;
+use Illuminate\Http\Request;
 
 class YooKassaCallbackController
 {
-    public function handle(YooKassaWebhookRequest $request)
+    // public function handle(YooKassaWebhookRequest $request)
+    public function handle(Request $request)
     {
-        $data = $request->validated();
-        info("WEBHOOK DATA" . json_encode($data));
+        info("WEBHOOK DATA" . $request->getContent());
     }
 }
