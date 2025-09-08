a9ab2f9c5d390c4fda7fb229e3b8ff9211ac85fd
Ivan Goryaev <5474n.sage@gmail.com>
2025-09-08 20:10:09 +0300

    добавил команду в консоль

--- app/Console/Commands/DeleteExpiredKeys.php
+++ app/Console/Commands/DeleteExpiredKeys.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Http\Enums\NotificationType;
 use App\Http\Integrations\RabbitMQService;
+use App\Http\Repositories\WireGuardRepository;
 use App\Models\Key;
 use Illuminate\Console\Command;
 
@@ -13,18 +14,23 @@ class DeleteExpiredKeys extends Command
 
     protected $description = 'Удаление ключей через неделю после истечения срока жизни';
 
-    public function handle(RabbitMQService $rabbitMQService)
-    {
+    public function handle(
+        RabbitMQService $rabbitMQService,
+        WireGuardRepository $wireGuardRepository,
+    ) {
         $keys = Key::where('expiration_date', '<=', now()->addWeek())
             ->with(['order.user', 'order.region']);
 
         if ($keys->exists()) {
-            $keys->cursor()->each(function (Key $key) use ($rabbitMQService) {
+            $keys->cursor()->each(function (Key $key) use ($rabbitMQService, $wireGuardRepository) {
                 $telegramId = $key->order->user->telegram_id;
 
                 $keyName = $key->config_name;
                 $regionName = $key->order->region->name;
                 $regionFlag = $key->order->region->flag;
+
+                $wireGuardRepository->deleteConfig($key->config_id);
+                $key->delete();
 
                 $rabbitMQService->publish('notifications', [
                     'telegram_id' => $telegramId,
@@ -35,8 +41,6 @@ class DeleteExpiredKeys extends Command
                         'region_flag' => $regionFlag,
                     ],
                 ]);
-
-                $key->delete();
             });
         }
     }
